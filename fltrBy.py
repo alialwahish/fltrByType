66452c92c6d70cecd87ec8e384fe6cad13efb01e
Ali Bayati <bayati.ali@icloud.com>
2018-04-30 19:10:59 -0500

    initial commit

---
+++ fltrBy.py
@@ -0,0 +1,21 @@
+
+x= [4,34,22,68,9,13,3,5]
+
+
+if type(x) is int:
+    if x >= 100:
+        print("that's a big number")
+    else:
+        print("that's a small number")
+if type(x) is str:
+    if len(x)>=50:
+        print("that's a long sentence")
+    else:
+        print("thats a short sentence")
+if type(x) is list:
+    if len(x) >= 10:
+        print("that's a big List")
+    else:
+        print("that's a small List")
+    
+
